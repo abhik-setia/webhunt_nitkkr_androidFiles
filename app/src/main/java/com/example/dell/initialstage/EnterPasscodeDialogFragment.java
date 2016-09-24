0371cd75906a667ce36bb2ed14c05c9edb234ed8
Abhik Setia <Abhik Setia>
2016-09-24 16:54:17 +0530

    made popup for passcode and tabbed activity

---
+++ app/src/main/java/com/example/dell/initialstage/EnterPasscodeDialogFragment.java
@@ -0,0 +1,59 @@
+package com.example.dell.initialstage;
+
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.app.DialogFragment;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.EditText;
+
+/**
+ * Created by Dell on 24-Sep-16.
+ */
+
+public class EnterPasscodeDialogFragment extends DialogFragment {
+
+    String event_name,passcode,user_entered_passcode;
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        event_name=getArguments().getString("event_name");
+        passcode=getArguments().getString("passcode");
+    }
+
+    public Dialog onCreateDialog(Bundle savedInstanceState){
+        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+        // Get the layout inflater
+        final LayoutInflater inflater = getActivity().getLayoutInflater();
+
+        // Inflate and set the layout for the dialog
+        // Pass null as the parent view because its going in the dialog layout
+        final View view=inflater.inflate(R.layout.popup_layout, null);
+        builder.setView(view)
+                // Add action buttons
+                .setPositiveButton("Enter", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int id) {
+
+                               EditText ed=(EditText)view.findViewById(R.id.passcode_editText);
+                        user_entered_passcode=ed.getText().toString();
+                        if(passcode.equals(user_entered_passcode)){
+                            //start activity
+                            startActivity(new Intent(getActivity().getApplicationContext(),EventRound.class));
+                            getActivity().finish();
+                        }else{
+                            //Toast to
+                        }
+                    }
+                })
+                .setNegativeButton("cancel", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        EnterPasscodeDialogFragment.this.getDialog().cancel();
+                    }
+                });
+        return builder.create();
+    }
+}
--- app/src/main/java/com/example/dell/initialstage/Rules_timer.java
+++ app/src/main/java/com/example/dell/initialstage/Rules_timer.java
@@ -1,5 +1,10 @@
 package com.example.dell.initialstage;
 
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.app.DialogFragment;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
@@ -8,7 +13,9 @@ import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.widget.Button;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
@@ -21,6 +28,7 @@ public class Rules_timer extends AppCompatActivity {
     String event_name,passcode, society, event_date, start_time, end_time;
     String rules_json;
     String[] rules_array;
+    private Button play_btn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -90,6 +98,30 @@ public class Rules_timer extends AppCompatActivity {
             myTextViews[i] = rowTextView;
 
         }
+
+
+        //This button will only be visible when event is online
+        play_btn=(Button)findViewById(R.id.play_btn);
+        play_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                if(!passcode.equals("")){
+                    Bundle b=new Bundle();
+                    b.putString("event_name",event_name);
+                    b.putString("passcode",passcode);
+                    DialogFragment newDialogFragment=new EnterPasscodeDialogFragment();
+                    newDialogFragment.setArguments(b);
+                    newDialogFragment.show(getFragmentManager(),"passcode_fragment");
+
+                }else{
+                    startActivity(new Intent(getApplicationContext(),EventRound.class));
+                    finish();
+                }
+            }
+        });
     }
+
+
 
 }
